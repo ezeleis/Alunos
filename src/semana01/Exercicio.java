6eca95061415e02507c721d6723d7189c8dd91fc
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-05-29 17:44:21 -0300

    feature(exercicio):ask for grades adn store in array

--- src/semana01/Exercicio.java
+++ src/semana01/Exercicio.java
@@ -1,20 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nomeAluno = "";
-        String[] alunos = new String[100];
-
-        int index = 0;
+        ArrayList<String> alunos = new ArrayList<>();
 
         while (!nomeAluno.equalsIgnoreCase("fim")) {
             System.out.print("Digite o nome do aluno (ou 'fim' para sair): ");
             nomeAluno = scanner.nextLine();
 
             if (!nomeAluno.equalsIgnoreCase("fim")) {
-                alunos[index] = nomeAluno;
-                index++;
+                alunos.add(nomeAluno);
                 System.out.println("Aluno: " + nomeAluno);
             }
         }
@@ -22,9 +20,31 @@ public class Exercicio {
         System.out.println("Programa encerrado.");
 
 
-        System.out.println("Lista de alunos:");
-        for (int i = 0; i < index; i++) {
-            System.out.println(alunos[i]);
+        double[][] notas = new double[alunos.size()][3];
+
+
+        for (int i = 0; i < alunos.size(); i++) {
+            String aluno = alunos.get(i);
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Digite a nota " + (j + 1) + " para " + aluno + ": ");
+                notas[i][j] = scanner.nextDouble();
+            }
+
+            scanner.nextLine();
+        }
+
+        System.out.println("Notas dos alunos:");
+        for (int i = 0; i < alunos.size(); i++) {
+            String aluno = alunos.get(i);
+            double[] grades = notas[i];
+
+            System.out.println("Aluno: " + aluno);
+            System.out.println("Notas:");
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Nota " + (j + 1) + ": " + grades[j]);
+            }
+            System.out.println();
         }
     }
 }
